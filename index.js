9a426c4365fde4667951be3ad4b706527c890851
Debashis Gayen <debashisgayen02@gmail.com>
2024-05-20 20:15:38 +0530

    update

--- index.js
+++ index.js
@@ -1,8 +1,36 @@
 const { spawn } = require("child_process");
-setInterval(() => {
+const express = require("express");
+const cors = require("cors");
+const app = express();
+const PORT = 3000;
+app.use(express.json());
+app.use(cors());
+
+app.get("/prediction", async (req, res) => {
+  let value = 0;
   const childProcess = spawn("python", [`indexChild.py`]);
+
+  let pythonOutput = "";
   childProcess.stdout.on("data", (data) => {
-    // pythonOutput += data.toString();
-    console.log(data.toString());
+    pythonOutput += data.toString();
   });
-}, 10000);
+
+  childProcess.on("close", (code) => {
+    if (code !== 0) {
+      return res.status(500).json({ error: "Python script error" });
+    }
+
+    try {
+      const result = JSON.parse(pythonOutput);
+      res.status(200).json(result);
+    } catch (error) {
+      res
+        .status(500)
+        .json({ error: "Failed to parse JSON from python script" });
+    }
+  });
+});
+
+app.listen(PORT, () => {
+  console.log(`server is running on: ${PORT}`);
+});
--- indexChild.py
+++ indexChild.py
@@ -6,6 +6,18 @@ import sys
 import matplotlib.pyplot as plt
 import statsmodels.api as sm
 import seaborn as sns
+
+
+def arima_forecast(df, column, order, steps):
+    # Fit ARIMA model
+    model = sm.tsa.arima.ARIMA(df[column], order=order)
+    model_fit = model.fit()
+
+    # Forecast
+    forecast = model_fit.forecast(steps=steps)
+    return forecast
+
+
 url = "https://deba0208-server.onrender.com/data"
 r = requests.get(url)
 data = r.json()
@@ -16,17 +28,33 @@ df.loc[:, ['nh4']] = np.where(df['nh4'] < 0, df['nh4'].median(), df['nh4'])
 df[["time", "date"]] = df["time"].str.split(" ", expand=True)
 df = df[["date", "time", "co2", "co", "nh4", "pm25",
          "TVOC", "Temperature", "Humidity", "AQI"]]
-model = sm.tsa.arima.ARIMA(df["co"], order=(10, 1, 10), dates=None)
-model_fit = model.fit()
-fore = model_fit.forecast(steps=100)
-new_co2 = list(df["co"])
-size = (fore.mean() * 3.5) / 2
-myList = [size] * (len(df["co2"])+100)
-print(fore)
-plt.plot(new_co2)
-plt.plot(myList, 'r')
-plt.plot(np.arange(len(df["co2"]), len(df["co2"])+100), fore, 'g')
-plt.show()
-plt.close()
-# print(df)
+df = df.tail(30)
+
+# model = sm.tsa.arima.ARIMA(df["co2"], order=(10, 1, 10), dates=None)
+# model_fit = model.fit()
+# fore = model_fit.forecast(steps=100)
+# size = (fore.mean() * 3.5) / 2
+# new_co2 = list(df["co"])
+# myList = [size] * (len(df["co2"])+100)
+forecast_steps = 100
+arima_order = (1, 1, 1)
+co2 = arima_forecast(df, "co2", arima_order, forecast_steps)
+aqi = arima_forecast(df, "AQI", arima_order, forecast_steps)
+tvoc = arima_forecast(df, "TVOC", arima_order, forecast_steps)
+
+co2 = (co2.mean() * 3.5) / 2
+aqi = (aqi.mean() * 3.5) / 2
+tvoc = (tvoc.mean() * 3.5) / 2
+data_to_send = {
+    "co2": co2,
+    "aqi": aqi,
+    "tvoc": tvoc
+}
+print(json.dumps(data_to_send))
 sys.stdout.flush()
+# plt.plot(new_co2)
+# plt.plot(myList, 'r')
+# plt.plot(np.arange(len(df["co2"]), len(df["co2"])+100), fore, 'g')
+# plt.show()
+# plt.close()
+# print(df)
